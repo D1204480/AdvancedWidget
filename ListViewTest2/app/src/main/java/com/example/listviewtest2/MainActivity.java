3305f08bed92c0bf0048de2a6ad4c62e9661ea34
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-08-07 11:15:33 +0800

    透過自訂arrayAdapter方法, 讓ListView的內容能自動變化

--- ListViewTest2/app/src/main/java/com/example/listviewtest2/MainActivity.java
+++ ListViewTest2/app/src/main/java/com/example/listviewtest2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.listviewtest2;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,7 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView cityListView;
+    private List<String> cities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +35,39 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             return insets;
         });
 
-        ListView listView = (ListView) findViewById(R.id.cities_listView);
-        listView.setOnItemClickListener(this);
+        // 註冊ListView監聽
+//        ListView listView = (ListView) findViewById(R.id.cities_listView);
+//        listView.setOnItemClickListener(this);
+
+        // 使用方法新增array裡的資料
+        cityListView = (ListView) findViewById(R.id.cities_listView);
+        setCities();
+        cityListView.setOnItemClickListener(this);
     }
+
+
+    private void setCities() {
+        cities = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));  // 取得array裡的資料
+        cities.add("Taichung");
+        cities.add("Hualien");
+        cities.add("Taitung");
+
+        // new ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities);
+        cityListView.setAdapter(adapter);
+    }
+
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
         TextView textView = (TextView) findViewById(R.id.textView);
-        String[] citiesArray = getResources().getStringArray(R.array.cities);   // 找到目標array裡的資料
+//        String[] citiesArray = getResources().getStringArray(R.array.cities);   // 找到目標array裡的資料
 
-        // 彈跳視窗
+        // Modal彈跳視窗
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("城市");
-        dialog.setMessage("您選擇的是: " + citiesArray[i]);
+        dialog.setMessage("您選擇的是: " + cities.get(i));
         dialog.setCancelable(true);
         dialog.setPositiveButton("確定", null);
         dialog.setNeutralButton("取消", null);
@@ -48,6 +75,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         dialog.show();
 
 //        textView.setText("您選擇的是: " + citiesArray[i]);
+
+        // Toast顯示視窗
 //        Toast.makeText(this, "您選擇的是: " + citiesArray[i], Toast.LENGTH_LONG).show();  // 顯示結果在畫面上
 
     }
